b25d578b248c44a7041b3948809b29481bb87c75
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2019-12-27 23:03:24 +0530

    minor change

--- testing_compression_engine.py
+++ testing_compression_engine.py
@@ -2,7 +2,12 @@ from util import load_single_image, normalize
 import cv2
 import sys
 import compression
+import datetime
 
 #image = cv2.imread(sys.argv[1])
 
+start = datetime.datetime.now()
+
 compression.compression_engine(sys.argv[1])
+
+print("DONE. time passed : ", ((datetime.datetime.now() - start).seconds) / 60, " minutes")
