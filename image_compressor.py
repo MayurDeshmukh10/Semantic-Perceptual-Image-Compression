f35aba95ac47080bf2ae471c8dc5366c0f64dce9
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2019-12-29 20:59:43 +0530

    Preserving original metadata of image after compression

--- image_compressor.py
+++ image_compressor.py
@@ -5,6 +5,7 @@ import compression
 from PIL import Image
 import datetime
 import os
+import piexif
 
 filename = sys.argv[1]
 
@@ -14,14 +15,13 @@ name = filename.split(".")[0]
 
 
 if extension == "tiff" or extension == "tif":
+    image_type = "tiff"
     start = datetime.datetime.now()
     im = Image.open(filename)
     converted = im.convert("RGB")
-    print("SIZE",im.size)
     converted.save("temp.png",'PNG',quality=100)
     test = Image.open("temp.png")
-    print("temp size : ",test.size)
-    compression.compression_engine("temp.png")
+    compression.compression_engine("temp.png","",image_type)
     out = Image.open("output/_compressed_temp.png_.jpg")
     out.save("__compressed__"+name+".tiff","TIFF")
     os.remove("temp.png")
@@ -30,7 +30,13 @@ if extension == "tiff" or extension == "tif":
 
 elif extension == "jpg" or extension == "jpeg" or extension == "png":
     start = datetime.datetime.now()
-    compression.compression_engine(filename)
+    im = Image.open(filename)
+    exif_dict = piexif.load(im.info["exif"])
+    w, h = im.size
+    exif_dict["0th"][piexif.ImageIFD.XResolution] = (w, 1)
+    exif_dict["0th"][piexif.ImageIFD.YResolution] = (h, 1)
+    exif_bytes = piexif.dump(exif_dict)
+    compression.compression_engine(filename,exif_bytes,"")
     print("DONE. time passed : ", ((datetime.datetime.now() - start).seconds) / 60, " minutes")
 
 
