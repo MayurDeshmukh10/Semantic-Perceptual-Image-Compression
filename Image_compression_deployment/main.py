4cda77b30db3876692366bdd3ce95c9435e4b2f4
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-04-13 19:15:16 +0530

    Added support for png and handled some exception

--- Image_compression_deployment/main.py
+++ Image_compression_deployment/main.py
@@ -18,52 +18,19 @@ def main(args):
 
 	headers = args["__ow_headers"]
 
-	print("AGAINadsd")
-
-
 	print("headers : ",headers)
+
+	content_type = headers['content-type']
+
+	print("Content-type : ",content_type)
 
 	imagedata = args['__ow_body']
 
 
-	#print("Image data : ",imagedata)
-	print("Image : ",imagedata)
-	
-
-
-	#base64_string = bytes(args['__ow_body'],'utf-8')
-	#converted = base64_string.decode('base64').strip()
-	
+	inputfile = ""
 
 
 
-
-
-
-	#with open("inputfile.jpg","wb") as f:
-	#	f.write(converted)
-
-	#print("BASE64 : ",base64_string)
-	#my_str_as_bytes = bytes(base64_string,'utf-8')
-
-	#print("AGAIN")
-
-	#decode_str = my_str_as_bytes.decode("base64")
-
-	#file_like = StringIO(base64_string	)
-
-	#img = Image.open(file_like)
-
-	#rgb_img = img.convert("RGB")
-
-	#rgb_img.save("inputfile.jpg","JPEG")
-
-	#my_str_as_bytes = bytes(base64_string,'utf-8')
-
-	#with open("inputfile.jpeg","wb") as fh:
-	#	fh.write(base64.decodebytes(my_str_as_bytes))
-
-	#fh.close()
 	print("AGAIN")
 	
 	data_bytes = imagedata.encode('ascii')
@@ -72,35 +39,35 @@ def main(args):
 
 	data = message_bytes.decode('ascii')
 
-	data_bytes = bytes(data,'utf-8')
-
-	with open("inputfile.jpg","wb") as f:
-		f.write(base64.decodebytes(data_bytes))
-
-
-
-	#image_data = BytesIO(converted)
-
-	#img = Image.open(image_data)
-
-	#img.save("input.jpg","JPEG")
-	#img_data = base64.b64decode(base64_string)
-
-
-	#print("NeW")
-	#decoded_str = decode_base64(imageData)
-
-	#print("Final : ",data)
-
-	#with open("inputfile.jpeg","w") as fh:
-	#	fh.write(base64_string)
+	print(data)
 	
-	print("SIZE OF FILE : ",os.stat('inputfile.jpg').st_size)
+	try:
+	
+		base64_image = data.split("base64,")[1]
+
+	except:
+		base64_image = data
+
+
+	data_bytes = bytes(base64_image,'utf-8')
+
+	if content_type == "image/jpg":
+		inputfile = "inputfile.jpg" 
+		with open(inputfile,"wb") as f:
+			f.write(base64.decodebytes(data_bytes))
+	else:
+		inputfile = "inputfile.png"
+		with open(inputfile,"wb") as f:
+			f.write(base64.decodebytes(data_bytes))
+
+
+	
+	print("SIZE OF FILE : ",os.stat(inputfile).st_size)
 
 	
 	print("FILES AFTER :",os.listdir())
 
-	compression_engine("inputfile.jpg")
+	compression_engine(inputfile)
 	#image_compression("input.jpg")
 
 
